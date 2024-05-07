9445e673f84b9a97b159d375248ad8a0af66f19f
ZexDev4 <158588614+ZexDev4@users.noreply.github.com>
2024-05-07 17:57:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,26 +14,16 @@ def download_tiktok():
     if not url:
         return jsonify({
             "code": 404,
-            "creator": "AmmarBN",
             "message": "Masukkan parameter URL"
         })
-    try:
-        api_response = requests.post('https://api.tikmate.app/api/lookup', data=json.dumps({'url': url}))
-        api_response.raise_for_status()
-        response = api_response.json()
-        username = response.get('author_name', '')
-        desc = response.get('desc', '')
-        create_up = response.get('create_time', '')
-        result_url = f"https://pride.nowmvideo.com/download/{response['token']}/{response['id']}.mp4"
-        return jsonify(
-            {
-                "username": username,
-                "description": desc,
-                "created_at": create_up,
-                "result": result_url
-            }
-        )
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": "Gagal terhubung dengan server Tikmate"}), 500
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    
+	response = requests.post('https://api.tikmate.app/api/lookup', data={'url': url}).json()
+	url_down = (f"https://pride.nowmvideo.com/download/{response['token']}/{response['id']}.mp4")
+	judul_desc = response['desc']
+	tanggal_up = response['create_time']
+	total_like = response['like_count']
+	total_share = response['share_count']
+	total_comen = response['comment_count']
+	nama_tt = response['author_name']
+	username_tt = response['author_id']
+	return jsonify({"nama": nama_tt, "username": username_tt, "desc": judul_desc, "tanggal_upload": tanggal_up, "total_like": total_like, "total_komen": total_comen, "total_share": total_share, "url_down": url_down})
