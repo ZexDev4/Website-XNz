e787a30a3adfdaf6078b35d3e7757943d0fc1edb
ZexDev4 <158588614+ZexDev4@users.noreply.github.com>
2024-05-07 18:33:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def download_tiktok():
             "code": 404,
             "message": "Masukkan parameter URL"
 	})
-    headers = {'Host': 'api.tikmate.app','sec-ch-ua': 'Google','sec-ch-ua-platform': 'Windows','sec-ch-ua-mobile': '?0','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.111 Safari/537.36','content-type': 'application/x-www-form-urlencoded','accept': '*/*','origin': 'https://tikmate.app','sec-fetch-site': 'same-site','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty',}
+    headers = {'Host': 'api.tikmate.app','sec-ch-ua': 'Google','sec-ch-ua-platform': 'Windows','sec-ch-ua-mobile': '?0','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.111 Safari/537.36','content-type': 'multipart/form-data; boundary=----WebKitFormBoundarylQlbfSRCFqG9RvRs','accept': '*/*','origin': 'https://tikmate.app','sec-fetch-site': 'same-site','sec-fetch-mode': 'cors','sec-fetch-dest': 'empty',}
 	
     response = requests.post('https://api.tikmate.app/api/lookup', data={'url': url}).text
     return jsonify({response})
